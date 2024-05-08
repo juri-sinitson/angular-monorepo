7872a2a739844aec8df056f9e846db8b0da6b0c0
Juri Sinitson <auto@sinitson.com>
2024-05-08 14:33:56 +0200

    refactor(deps): bump up deps with nx
    - Commands used:
    `npx nx migrate latest`
    `pnpm install --no-frozen-lockfile`
    `pnpm exec nx migrate --run-migrations`
    
    - See also:
    https://nx.dev/recipes/tips-n-tricks/advanced-update#updating-to-the-latest-nx-version

--- spot-check.sh
+++ spot-check.sh
@@ -3,4 +3,5 @@ pnpm nx run shared-business-examples:test --parallel=true --skip-nx-cache && \
 pnpm nx run examples-frontend:build --parallel=true --skip-nx-cache && \
 pnpm start-server-and-test ci:helper:start-backend http://localhost:3000 \
   'pnpm nx run examples-frontend-e2e:e2e --parallel=true --skip-nx-cache' && \
-pnpm nx run shared-ui-menu:test-storybook --parallel=true --skip-nx-cache
+pnpm pnpm start-server-and-test ci:helper:storybook http://localhost:4400 \
+  'nx run shared-ui-menu:test-storybook --parallel=true --skip-nx-cache';
