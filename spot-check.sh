45fd431fb026a1ce51431eb63b2ee398cd177975
Juri Sinitson <auto@sinitson.com>
2024-05-19 21:18:00 +0200

    chore(tests): update build command for spot-check script

--- spot-check.sh
+++ spot-check.sh
@@ -1,7 +1,7 @@
-pnpm nx run examples-backend:build --parallel=true --skip-nx-cache && \
+pnpm nx run central-backend:build --parallel=true --skip-nx-cache && \
 pnpm nx run shared-business-examples:test --parallel=true --skip-nx-cache && \
 pnpm nx run examples-frontend:build --parallel=true --skip-nx-cache && \
 pnpm start-server-and-test ci:helper:start-backend http://localhost:3000 \
   'pnpm nx run examples-frontend-e2e:e2e --parallel=true --skip-nx-cache' && \
 pnpm pnpm start-server-and-test ci:helper:storybook http://localhost:4400 \
-  'nx run shared-ui-menu:test-storybook --parallel=true --skip-nx-cache';
+  'nx run shared-business-feature-examples:test-storybook --parallel=true --skip-nx-cache';
