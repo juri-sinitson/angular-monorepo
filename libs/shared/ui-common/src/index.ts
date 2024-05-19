be3ad9d0d16aae958ec16a9220d7dee2f651dd8e
Juri Sinitson <auto@sinitson.com>
2024-05-19 02:41:00 +0200

    refactor(shared-ui-common): exporting card component

--- libs/shared/ui-common/src/index.ts
+++ libs/shared/ui-common/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/components/basic-table/basic-table.component';
 export * from './lib/components/common-wrapper/common-wrapper.component';
 export * from './lib/abstracts/components/entities-list.abstract.component';
 export * from './lib/abstracts/components/entity-form.abstract.component';
+export * from './lib/components/card/card.component';
