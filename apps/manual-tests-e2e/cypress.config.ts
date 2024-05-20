8b50da26048d233b512197638dbb999aea0c6296
Juri Sinitson <auto@sinitson.com>
2024-05-20 13:22:17 +0200

    ci(test): #50, #51 improve and fix the CI pipeline

--- apps/manual-tests-e2e/cypress.config.ts
+++ apps/manual-tests-e2e/cypress.config.ts
@@ -2,9 +2,18 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 import { defineConfig } from 'cypress';
 
+const port = 4203;
+
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename, { cypressDir: 'src' }),
-    baseUrl: 'http://localhost:4200',
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: `nx run persons-management-frontend:serve:development --port ${port}`,
+        production: `nx run persons-management-frontend:serve:production --port ${port}`,
+      },
+      ciWebServerCommand: `nx run persons-management-frontend:serve:production --port ${port}`,
+    }),
+    baseUrl: `http://localhost:${port}`,
   },
 });
