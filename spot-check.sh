92b19414d1818b2dea9e0a4e77f0f9f69b60740c
Juri Sinitson <auto@sinitson.com>
2024-05-31 16:00:17 +0200

    chore(spot-tests): adjust spot-tests, spot-tests pass
    - Spot tests pass, so we can be 99,9% sure
    the bump up in the commits below works

--- spot-check.sh
+++ spot-check.sh
@@ -1,7 +1,15 @@
+pnpm nx affected -t lint --parallel=true && \
 pnpm nx run central-backend:build --parallel=true --skip-nx-cache && \
 pnpm nx run shared-business-examples:test --parallel=true --skip-nx-cache && \
 pnpm nx run examples-frontend:build --parallel=true --skip-nx-cache && \
-pnpm start-server-and-test ci:helper:start-backend http://localhost:3000 \
-  'pnpm nx run examples-frontend-e2e:e2e --parallel=true --skip-nx-cache' && \
-pnpm pnpm start-server-and-test ci:helper:storybook http://localhost:4400 \
-  'nx run shared-business-feature-examples:test-storybook --parallel=true --skip-nx-cache';
+
+pnpm ci:storybook && \
+pnpm ci:helper:wait-for-storybook && \
+pnpm nx run shared-business-feature-examples:test-storybook --parallel=true --skip-nx-cache -- -u && \
+pnpm ci:kill-storybook && \
+
+pnpm build-backend && \
+pnpm ci:start-backend && \
+pnpm ci:helper:wait-for-backend && \
+pnpm nx run examples-frontend-e2e:e2e --parallel=true --skip-nx-cache && \
+pnpm ci:kill-backend
