d137a611647c5de9edf46af13ccd6c5f82e56703
Juri Sinitson <auto@sinitson.com>
2024-05-31 15:26:53 +0200

    refactor(deps): bump up nx to 19.1
    - Includes other updates, especially angular to 18.x
    - Commands used:
      `pnpm nx migrate latest`
      `pnpm install --no-frozen-lockfile`
      `pnpm exec nx migrate --run-migrations`
    
    See also:
    https://nx.dev/features/automate-updating-dependencies
    
     - `libs/shared/util-common/src/lib/util/app-config.util.ts` was adjusted
     manually to use the provider of HTTP from angular 18.

--- libs/shared/util-common/src/lib/util/app-config.util.ts
+++ libs/shared/util-common/src/lib/util/app-config.util.ts
@@ -1,10 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const commonAppConfig: ApplicationConfig = { 
   providers: [
     provideAnimations(),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
   ],
 }
