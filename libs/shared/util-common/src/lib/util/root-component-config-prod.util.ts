354c1a9ff80e0b07e7198626c9ce27fbd5917482
Juri Sinitson <auto@sinitson.com>
2024-06-01 18:37:24 +0200

    refactor(angular): turn off zoneless for now to stay production ready
    - We tested zoneless, and it seems to work in general
    (currently except of the storybook interaction tests).

--- libs/shared/util-common/src/lib/util/root-component-config-prod.util.ts
+++ libs/shared/util-common/src/lib/util/root-component-config-prod.util.ts
@@ -1,13 +1,18 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { 
+  ApplicationConfig, 
+  // Deactivate zoneless because it's not yes production ready.
+  // provideExperimentalZonelessChangeDetection 
+} from '@angular/core';
 import { rootComponentConfigBase } from './root-component-config-base.util';
 
 export const rootComponentConfigProd: ApplicationConfig = { 
   providers: [
     ...rootComponentConfigBase.providers,
     provideHttpClient(),
+    // For now deactivated because it's not yet production ready.
     // TODO! Move to base config when stable.
     // Read the comments in the base config for more information.
-    provideExperimentalZonelessChangeDetection(),
+    // provideExperimentalZonelessChangeDetection(),
   ],
 }
