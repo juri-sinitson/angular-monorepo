e9473bbf580e536b0ce9a25c43984c5f991cd3c8
Juri Sinitson <auto@sinitson.com>
2024-06-24 14:20:17 +0200

    refactor(tests): adjust the spot check commands

--- spot-check.sh
+++ spot-check.sh
@@ -1,15 +1,8 @@
 pnpm nx affected -t lint --parallel=true && \
-pnpm nx run central-backend:build --parallel=true --skip-nx-cache && \
-pnpm nx run shared-business-examples:test --parallel=true --skip-nx-cache && \
-pnpm nx run examples-frontend:build --parallel=true --skip-nx-cache && \
-
-pnpm ci:storybook && \
-pnpm ci:helper:wait-for-storybook && \
-pnpm nx run shared-business-feature-examples:test-storybook --parallel=true --skip-nx-cache -- -u && \
-pnpm ci:kill-storybook && \
-
-pnpm build-backend && \
-pnpm ci:start-backend && \
-pnpm ci:helper:wait-for-backend && \
-pnpm nx run examples-frontend-e2e:e2e --parallel=true --skip-nx-cache && \
-pnpm ci:kill-backend
+pnpm build-backend -- --parallel=true --skip-nx-cache && \
+pnpm nx run persons-management-api-persons:test --parallel=true --skip-nx-cache && \
+pnpm build-frontend --skip-nx-cache && \
+pnpm start-server-and-test start-backend http://localhost:3000 \
+  'pnpm e2e-frontend --parallel=true --skip-nx-cache' && \
+pnpm start-server-and-test 'pnpm storybook -- --ci' http://localhost:4400 \
+  'nx run persons-management-feature-birthdays:test-storybook --ci --parallel=true --skip-nx-cache';
